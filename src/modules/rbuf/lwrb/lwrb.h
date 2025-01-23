fdcd58a5bb10ff3f648e77085018954ce5f4593a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-01-23 23:29:21 +0600

    Rewrite rbuf_ctx and rbuf object with garbage collection support

--- src/modules/rbuf/lwrb/lwrb.h
+++ src/modules/rbuf/lwrb/lwrb.h
@@ -3,18 +3,3 @@
 #include "config.h" // IWYU pragma: keep
 #include "types.h" // IWYU pragma: keep
 #include "steroids/rbuf.h"
-
-static st_modfuncentry_t st_module_rbuf_lwrb_funcs[] = {
-    {"init"          , st_rbuf_init},
-    {"quit"          , st_rbuf_quit},
-    {"create"        , st_rbuf_create},
-    {"destroy"       , st_rbuf_destroy},
-    {"push"          , st_rbuf_push},
-    {"peek"          , st_rbuf_peek},
-    {"pop"           , st_rbuf_pop},
-    {"drop"          , st_rbuf_drop},
-    {"clear"         , st_rbuf_clear},
-    {"get_free_space", st_rbuf_get_free_space},
-    {"is_empty"      , st_rbuf_is_empty},
-    {NULL, NULL},
-};
--- src/modules/rbuf/lwrb/types.h
+++ src/modules/rbuf/lwrb/types.h
@@ -11,14 +11,16 @@
 #include "steroids/types/modules/logger.h"
 #include "steroids/types/object.h"
 
-ST_MODCTX (
+typedef struct {
+    st_modctx_t;
     struct st_loggerctx_s *logger_ctx;
-) st_rbufctx_t;
+} st_rbufctx_t;
 
-ST_CLASS (
+typedef struct {
+    st_object_t;
     lwrb_t   handle;
     uint8_t  data[];
-) st_rbuf_t;
+} st_rbuf_t;
 
 #define ST_RBUFCTX_T_DEFINED
 #define ST_RBUF_T_DEFINED
