6c23c3054ac9c6be2ccc89de798268d20f45b156
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-02-06 03:15:50 +0600

    Rewrite fnv1a module with reference counting

--- include/steroids/fnv1a.h
+++ include/steroids/fnv1a.h
@@ -1,19 +1,6 @@
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
-
 #include "steroids/module.h"
 #include "steroids/types/modules/fnv1a.h"
 
 static st_fnv1actx_t *st_fnv1a_init(struct st_loggerctx_s *logger_ctx);
-static void st_fnv1a_quit(st_fnv1actx_t *fnv1a_ctx);
-
-static uint32_t st_fnv1a_u32hash_string(st_fnv1actx_t *fnv1a_ctx,
- const char *str);
-static uint32_t st_fnv1a_u32hash_bytes(st_fnv1actx_t *fnv1a_ctx,
- const void *ptr, size_t size);
-static st_u32hashstrfunc_t st_fnv1a_get_u32hashstr_func(
- st_fnv1actx_t *fnv1a_ctx);
-static st_u32hashbytesfunc_t st_fnv1a_get_u32hashbytes_func(
- st_fnv1actx_t *fnv1a_ctx);
--- src/modules/fnv1a/simple/simple.h
+++ src/modules/fnv1a/simple/simple.h
@@ -3,13 +3,3 @@
 #include "config.h" // IWYU pragma: keep
 #include "types.h" // IWYU pragma: keep
 #include "steroids/fnv1a.h"
-
-static st_modfuncentry_t st_module_fnv1a_simple_funcs[] = {
-    {"init"                 , st_fnv1a_init},
-    {"quit"                 , st_fnv1a_quit},
-    {"u32hash_string"       , st_fnv1a_u32hash_string},
-    {"u32hash_bytes"        , st_fnv1a_u32hash_bytes},
-    {"get_u32hashstr_func"  , st_fnv1a_get_u32hashstr_func},
-    {"get_u32hashbytes_func", st_fnv1a_get_u32hashbytes_func},
-    {NULL, NULL},
-};
