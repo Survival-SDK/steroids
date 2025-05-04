5e65a47c6dd0d161309b4f6b28f8306b7a1909b2
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-05 04:54:30 +0600

    Rewrite htable module with ms-extensions

--- src/modules/htable/hash_table/types.h
+++ src/modules/htable/hash_table/types.h
@@ -1,23 +1,24 @@
 #pragma once
 
+#include "steroids/types/modctx.h"
 #include "steroids/types/modules/logger.h"
 #include "steroids/types/object.h"
 
 typedef void (*st_freefunc_t)(void *ptr);
 
-ST_MODCTX (
+typedef struct {
+    st_modctx_t;
     struct st_loggerctx_s *logger_ctx;
-) st_htablectx_t;
+} st_htablectx_t;
 
-ST_CLASS (
+typedef struct {
+    st_object_t;
     struct hash_table *handle;
     st_freefunc_t      keydelfunc;
     st_freefunc_t      valdelfunc;
-) st_htable_t;
+} st_htable_t;
 
-ST_CLASS (
-    struct hash_entry *handle;
-) st_htiter_t;
+typedef st_object_t st_htiter_t;
 
 #define ST_HTABLECTX_T_DEFINED
 #define ST_FREEFUNC_T_DEFINED
