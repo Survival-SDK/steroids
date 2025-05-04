02195fae35362e50e38a96adf7929d62a99cebc6
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-05 04:54:30 +0600

    Rewrite htable module with ms-extensions

--- include/steroids/htable.h
+++ include/steroids/htable.h
@@ -4,21 +4,3 @@
 #include "steroids/types/modules/htable.h"
 
 static st_htablectx_t *st_htable_init(struct st_loggerctx_s *logger_ctx);
-static void st_htable_quit(st_htablectx_t *htable_ctx);
-
-static st_htable_t *st_htable_create(st_htablectx_t *htable_ctx,
- st_u32hashfunc_t hashfunc, st_keyeqfunc_t keyeqfunc, st_freefunc_t keydelfunc,
- st_freefunc_t valdelfunc);
-static void st_htable_destroy(st_htable_t *htable);
-static bool st_htable_insert(st_htable_t *htable, st_htiter_t *iter,
- const void *key, void *value);
-static void *st_htable_get(st_htable_t *htable, const void *key);
-static bool st_htable_remove(st_htable_t *htable, const void *key);
-static void st_htable_clear(st_htable_t *htable);
-static bool st_htable_contains(st_htable_t *htable, const void *key);
-static bool st_htable_find(st_htable_t *htable, st_htiter_t *dst,
- const void *key);
-static bool st_htable_first(st_htable_t *htable, st_htiter_t *dst);
-static bool st_htable_next(st_htiter_t *current, st_htiter_t *dst);
-static const void *st_htable_get_iter_key(const st_htiter_t *iter);
-static void *st_htable_get_iter_value(const st_htiter_t *iter);
--- src/modules/htable/hash_table/hash_table.h
+++ src/modules/htable/hash_table/hash_table.h
@@ -3,21 +3,3 @@
 #include "config.h" // IWYU pragma: keep
 #include "types.h" // IWYU pragma: keep
 #include "steroids/htable.h"
-
-static st_modfuncentry_t st_module_htable_hash_table_funcs[] = {
-    {"init"          , st_htable_init},
-    {"quit"          , st_htable_quit},
-    {"create"        , st_htable_create},
-    {"destroy"       , st_htable_destroy},
-    {"insert"        , st_htable_insert},
-    {"get"           , st_htable_get},
-    {"remove"        , st_htable_remove},
-    {"clear"         , st_htable_clear},
-    {"contains"      , st_htable_contains},
-    {"find"          , st_htable_find},
-    {"first"         , st_htable_first},
-    {"next"          , st_htable_next},
-    {"get_iter_key"  , st_htable_get_iter_key},
-    {"get_iter_value", st_htable_get_iter_value},
-    {NULL, NULL},
-};
