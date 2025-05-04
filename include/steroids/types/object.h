de2cfe2f42353518af3bb0570c473f8b52bc3dd6
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-05 04:51:38 +0600

    Add userdata field and fake destructor implementation
    For objects created on stack

--- include/steroids/types/object.h
+++ include/steroids/types/object.h
@@ -8,6 +8,7 @@
 struct st_object;
 
 typedef void (*st_object_dtor_t)(void *obj);
+
 typedef struct st_object *(*st_object_get_owner_t)(void *obj);
 typedef void (*st_object_destroy_t)(void *obj);
 
@@ -20,6 +21,7 @@ typedef struct st_object {
     const st_object_funcs_t *funcs;
     struct st_object        *st_owner;
     st_object_dtor_t         st_dtor;
+    uintptr_t                st_userdata;
 } st_object_t;
 
 static st_object_t *st_object_get_owner(void *obj);
@@ -38,6 +40,7 @@ static st_object_t *st_object_init(st_object_t *obj, const void *funcs,
     obj->funcs    = funcs ?: &st_object_funcs;
     obj->st_dtor  = dtor;
     obj->st_owner = owner;
+    obj->st_userdata = 0ul;
 
     return obj;
 }
@@ -65,4 +68,9 @@ static void st_object_destroy(void *obj) {
         ((st_object_t *)obj)->st_dtor(obj);
     else
         free(obj);
+}
+
+/* For stack objects, created with placement new */
+static void st_object_fake_dtor(void *obj) {
+    /* noop */
 }
