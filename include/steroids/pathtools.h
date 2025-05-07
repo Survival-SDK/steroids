0690d8b186a2ee28bbf08015476c09657631d0fe
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-07 06:21:29 +0600

    Rewrite pathtools with ms-extensions

--- include/steroids/pathtools.h
+++ include/steroids/pathtools.h
@@ -1,16 +1,6 @@
 #pragma once
 
-#include <stddef.h>
-
 #include "steroids/module.h"
 #include "steroids/types/modules/pathtools.h"
 
 static st_pathtoolsctx_t *st_pathtools_init(struct st_loggerctx_s *logger_ctx);
-static void st_pathtools_quit(st_pathtoolsctx_t *pathtools_ctx);
-
-static bool st_pathtools_resolve(st_pathtoolsctx_t *pathtools_ctx, char *dst,
- size_t dstsize, const char *path);
-static bool st_pathtools_get_parent_dir(st_pathtoolsctx_t *pathtools_ctx,
- char *dst, size_t dstsize, const char *path);
-static bool st_pathtools_concat(st_pathtoolsctx_t *pathtools_ctx, char *dst,
- size_t dstsize, const char *path, const char *append);
