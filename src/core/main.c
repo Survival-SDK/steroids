7500118705fd0abb07481cee77c97087c9524099
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-07 06:44:24 +0600

    Rewrite fs module with ms-extensions

--- src/core/main.c
+++ src/core/main.c
@@ -4,7 +4,7 @@
 
 #include "modules_manager.h"
 
-// #include "steroids/types/modules/fs.h"
+#include "steroids/types/modules/fs.h"
 #include "steroids/types/modules/ini.h"
 #include "steroids/types/modules/logger.h"
 #include "steroids/types/modules/opts.h"
@@ -15,7 +15,7 @@
 // #include "steroids/types/modules/spcpaths.h"
 // #include "steroids/types/modules/zip.h"
 
-// static st_fs_init_t     st_fs_init;
+static st_fs_init_t     st_fs_init;
 static st_ini_init_t    st_ini_init;
 static st_logger_init_t st_logger_init;
 static st_opts_init_t   st_opts_init;
@@ -40,7 +40,7 @@ static st_pathtools_init_t st_pathtools_init;
 
 static bool init_ctors(st_modsmgr_t *modsmgr,
  struct st_loggerctx_s *logger_ctx) {
-    // LOAD_CTOR(fs);
+    LOAD_CTOR(fs);
     LOAD_CTOR(ini);
     LOAD_CTOR(opts);
 //     LOAD_CTOR(runner);
@@ -55,7 +55,7 @@ static bool init_ctors(st_modsmgr_t *modsmgr,
 
 int main(int argc, char **argv) {
     st_modsmgr_t          *modsmgr = st_modsmgr_init();
-    // st_fsctx_t            *fs_ctx;
+    st_fsctx_t            *fs_ctx;
     st_inictx_t           *ini_ctx;
     struct st_loggerctx_s *logger_ctx;
     st_optsctx_t          *opts_ctx;
@@ -87,7 +87,7 @@ int main(int argc, char **argv) {
     ini_ctx = st_ini_init(logger_ctx);
     opts_ctx = st_opts_init(argc, argv, logger_ctx);
     pathtools_ctx = st_pathtools_init(logger_ctx);
-    // fs_ctx = st_fs_init(logger_ctx, pathtools_ctx);
+    fs_ctx = st_fs_init(logger_ctx, pathtools_ctx);
     // so_ctx = st_so_init(logger_ctx);
     // spcpaths_ctx = st_spcpaths_init(logger_ctx);
     // zip_ctx = st_zip_init(fs_ctx, logger_ctx, pathtools_ctx);
@@ -102,7 +102,7 @@ int main(int argc, char **argv) {
     // ST_ZIPCTX_CALL(zip_ctx, quit);
     // ST_SPCPATHSCTX_CALL(spcpaths_ctx, quit);
     // ST_SOCTX_CALL(so_ctx, quit);
-    // ST_FSCTX_CALL(fs_ctx, quit);
+    ST_FSCTX_CALL(fs_ctx, destroy);
     ST_PATHTOOLSCTX_CALL(pathtools_ctx, destroy);
     ST_OPTSCTX_CALL(opts_ctx, destroy);
     ST_INICTX_CALL(ini_ctx, destroy);
--- src/modules/fs/simple/simple.h
+++ src/modules/fs/simple/simple.h
@@ -3,11 +3,3 @@
 #include "config.h" // IWYU pragma: keep
 #include "types.h" // IWYU pragma: keep
 #include "steroids/fs.h"
-
-static st_modfuncentry_t st_module_fs_simple_funcs[] = {
-    {"init"         , st_fs_init},
-    {"quit"         , st_fs_quit},
-    {"get_file_type", st_fs_get_file_type},
-    {"mkdir"        , st_fs_mkdir},
-    {NULL, NULL},
-};
