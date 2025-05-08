472c85a00204b5011d1a0912cbcae7da7f5c729a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-08 19:12:38 +0600

    Rewrite spcpaths module with ms-extensions

--- include/steroids/spcpaths.h
+++ include/steroids/spcpaths.h
@@ -6,10 +6,3 @@
 #include "steroids/types/modules/spcpaths.h"
 
 static st_spcpathsctx_t *st_spcpaths_init(struct st_loggerctx_s *logger_ctx);
-static void st_spcpaths_quit(st_spcpathsctx_t *spcpaths_ctx);
-static void st_spcpaths_get_config_path(st_spcpathsctx_t *spcpaths_ctx,
- char *dst, size_t dstlen, const char *appname);
-static void st_spcpaths_get_data_path(st_spcpathsctx_t *spcpaths_ctx, char *dst,
- size_t dstlen, const char *appname);
-static void st_spcpaths_get_cache_path(st_spcpathsctx_t *spcpaths_ctx,
- char *dst, size_t dstlen, const char *appname);
--- src/modules/spcpaths/cfgpath/cfgpath.h
+++ src/modules/spcpaths/cfgpath/cfgpath.h
@@ -3,12 +3,3 @@
 #include "config.h" // IWYU pragma: keep
 #include "types.h" // IWYU pragma: keep
 #include "steroids/spcpaths.h"
-
-static st_modfuncentry_t st_module_spcpaths_cfgpath_funcs[] = {
-    {"init"           , st_spcpaths_init},
-    {"quit"           , st_spcpaths_quit},
-    {"get_config_path", st_spcpaths_get_config_path},
-    {"get_data_path"  , st_spcpaths_get_data_path},
-    {"get_cache_path" , st_spcpaths_get_cache_path},
-    {NULL, NULL},
-};
