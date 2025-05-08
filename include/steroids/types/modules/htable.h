354fb2a616a31dd5d7d582776c7d3a0679613b1d
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-05-08 18:44:07 +0600

    Remove unneeded destructors types

--- include/steroids/types/modules/htable.h
+++ include/steroids/types/modules/htable.h
@@ -21,11 +21,9 @@ typedef bool (*st_keyeqfunc_t)(const void *left, const void *right);
 #endif
 
 typedef st_htablectx_t *(*st_htable_init_t)(struct st_loggerctx_s *logger_ctx);
-typedef void (*st_htable_quit_t)(st_htablectx_t *htable_ctx);
 typedef st_htable_t *(*st_htable_create_t)(st_htablectx_t *htable_ctx,
  st_u32hashfunc_t hashfunc, st_keyeqfunc_t keyeqfunc, st_freefunc_t keydelfunc,
  st_freefunc_t valdelfunc);
-typedef void (*st_htable_destroy_t)(st_htable_t *htable);
 typedef bool (*st_htable_insert_t)(st_htable_t *htable, st_htiter_t *iter,
  const void *key, void *value);
 typedef void *(*st_htable_get_t)(st_htable_t *htable, const void *key);
