74f0531b465b2b85575d7c4c58a6e9b0facad92a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-12-16 19:55:21 +0600

    Unificate bindings sources and build process
    Now We need only 2-line CMakeLists.txt and luajit-code for every binding.
    No additional files hell.

--- src/modules/luajitbind/types.h
+++ src/modules/luajitbind/types.h
@@ -4,11 +4,10 @@
 #include "steroids/modules/logger.h"
 #include "steroids/modules/luajit.h"
 
-typedef struct st_luajitbindctx_s {
+typedef struct {
     st_modctx_t;
     st_loggerctx_t *logger_ctx;
     st_luajitctx_t *luajit_ctx;
 } st_luajitbindctx_t;
 
 #define ST_LUAJITBINDCTX_T_DEFINED
-
