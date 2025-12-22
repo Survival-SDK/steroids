5142a8c522f821b217ee5e374340cd60de134748
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-12-22 22:51:40 +0600

    Update gfxctx_egl module and binding

--- include/steroids/modules/gfxctx.h
+++ include/steroids/modules/gfxctx.h
@@ -1,12 +1,17 @@
 #pragma once
 
-#include "steroids/module.h"
+#include <stdbool.h>
+
+#include "steroids/modctx.h"
 #include "steroids/modules/monitor.h"
 #include "steroids/modules/window.h"
 #include "steroids/object.h"
 
+#ifndef ST_GFXCTXCTX_T_DEFINED
+    typedef st_modctx_t st_gfxctxctx_t;
+#endif
 #ifndef ST_GFXCTX_T_DEFINED
-    typedef struct st_gfxctx_s st_gfxctx_t;
+    typedef st_object_t st_gfxctx_t;
 #endif
 
 typedef enum {
@@ -40,20 +45,15 @@ typedef enum {
     ST_GAPI_UNKNOWN = ST_GAPI_MAX,
 } st_gapi_t;
 
-typedef st_modctx_t *(*st_gfxctx_init_t)(st_modctx_t *logger_ctx,
- st_modctx_t *monitor_ctx);
-typedef void (*st_gfxctx_quit_t)(st_modctx_t *gfxctx_ctx);
-
-typedef st_gfxctx_t *(*st_gfxctx_create_t)(st_modctx_t *gfxctx_ctx,
+typedef st_gfxctx_t *(*st_gfxctx_create_t)(st_gfxctxctx_t *gfxctx_ctx,
  st_monitor_t *monitor, st_window_t *window, st_gapi_t api);
-typedef st_gfxctx_t *(*st_gfxctx_create_shared_t)(st_modctx_t *gfxctx_ctx,
+typedef st_gfxctx_t *(*st_gfxctx_create_shared_t)(st_gfxctxctx_t *gfxctx_ctx,
  st_monitor_t *monitor, st_window_t *window, st_gfxctx_t *other);
 typedef bool (*st_gfxctx_make_current_t)(st_gfxctx_t *gfxctx);
 typedef bool (*st_gfxctx_swap_buffers_t)(st_gfxctx_t *gfxctx);
 typedef st_window_t *(*st_gfxctx_get_window_t)(st_gfxctx_t *gfxctx);
-typedef st_gapi_t (*st_gfxctx_get_api_t)(st_gfxctx_t *gfxctx);
+typedef st_gapi_t (*st_gfxctx_get_api_t)(const st_gfxctx_t *gfxctx);
 typedef unsigned (*st_gfxctx_get_shared_index_t)(const st_gfxctx_t *gfxctx);
-typedef void (*st_gfxctx_destroy_t)(st_gfxctx_t *gfxctx);
 typedef bool (*st_gfxctx_debug_enabled_t)(const st_gfxctx_t *gfxctx);
 typedef void (*st_gfxctx_set_userdata_t)(const st_gfxctx_t *gfxctx,
  const char *key, uintptr_t value);
@@ -61,22 +61,26 @@ typedef bool (*st_gfxctx_get_userdata_t)(const st_gfxctx_t *gfxctx,
  uintptr_t *dst, const char *key);
 
 typedef struct {
-    st_gfxctx_quit_t          quit;
+    st_modctx_funcs_t;
     st_gfxctx_create_t        create;
     st_gfxctx_create_shared_t create_shared;
 } st_gfxctxctx_funcs_t;
 
 typedef struct {
+    st_object_funcs_t;
     st_gfxctx_make_current_t     make_current;
     st_gfxctx_swap_buffers_t     swap_buffers;
     st_gfxctx_get_window_t       get_window;
     st_gfxctx_get_api_t          get_api;
     st_gfxctx_get_shared_index_t get_shared_index;
-    st_gfxctx_destroy_t          destroy;
     st_gfxctx_debug_enabled_t    debug_enabled;
     st_gfxctx_set_userdata_t     set_userdata;
     st_gfxctx_get_userdata_t     get_userdata;
 } st_gfxctx_funcs_t;
 
+#define ST_GFXCTXCTX_CALL(ctx, func, ...) \
+    ((st_gfxctxctx_funcs_t *)((const st_object_t *)ctx)->funcs)->func(ctx, \
+     ## __VA_ARGS__)
 #define ST_GFXCTX_CALL(object, func, ...) \
-    ((st_gfxctx_funcs_t *)((const st_object_t *)object)->funcs)->func(object, ## __VA_ARGS__)
+    ((st_gfxctx_funcs_t *)((const st_object_t *)object)->funcs)->func(object, \
+     ## __VA_ARGS__)
--- src/modules/gfxctx/egl/types.h
+++ src/modules/gfxctx/egl/types.h
@@ -3,57 +3,36 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 
+#include "steroids/modctx.h"
+#include "steroids/modsmgr.h"
 #include "steroids/modules/fnv1a.h"
 #include "steroids/modules/htable.h"
 #include "steroids/modules/logger.h"
+#include "steroids/modules/monitor.h"
 #include "steroids/modules/window.h"
 #include "steroids/object.h"
 
 #include "dlist.h"
 
 typedef struct {
-    st_modctx_t        *ctx;
-    st_logger_debug_t   debug;
-    st_logger_info_t    info;
-    st_logger_warning_t warning;
-    st_logger_error_t   error;
-} st_gfxctx_egl_logger_t;
-
-typedef struct {
-    st_modctx_t *ctx;
-} st_gfxctx_egl_monitor_t;
-
-typedef struct {
-    st_fnv1a_get_u32hashstr_func_t get_u32hashstr_func;
-} st_gfxctx_egl_fnv1a_t;
-
-typedef struct {
-    st_modctx_t       *ctx;
-    st_htable_init_t   init;
-    st_htable_quit_t   quit;
-    st_htable_create_t create;
-} st_gfxctx_egl_htable_t;
-
-typedef struct {
-    st_gfxctx_egl_logger_t  logger;
-    st_gfxctx_egl_monitor_t monitor;
-    st_gfxctx_egl_fnv1a_t   fnv1a;
-    st_gfxctx_egl_htable_t  htable;
-    bool                    debug_enabled;
-    EGLint                (*egl_debug_message_control_khr)(
+    st_modctx_t;
+    st_modsmgr_t     *modsmgr;
+    st_loggerctx_t   *logger_ctx;
+    st_monitorctx_t  *monitor_ctx;
+    st_windowctx_t   *window_ctx;
+    st_fnv1actx_t    *fnv1a_ctx;
+    st_htablectx_t   *htable_ctx;
+    bool              debug_enabled;
+    EGLint          (*egl_debug_message_control_khr)(
      EGLDEBUGPROCKHR callback, const EGLAttrib *attrib_list);
-    EGLint                (*egl_label_object_khr)(EGLDisplay display,
+    EGLint          (*egl_label_object_khr)(EGLDisplay display,
      EGLenum objectType, EGLObjectKHR object, EGLLabelKHR label);
-    bool                    must_quit;
-    size_t                  gfxctxs_count;
-} st_gfxctx_egl_t;
+    bool              must_quit;
+    size_t            gfxctxs_count;
+} st_gfxctxctx_t;
 
 typedef struct {
-    struct st_gfxctx_s *ctx;
-    unsigned            index;
-} st_gfxctx_shared_data_t;
-
-ST_STRUCT_CLASS(st_gfxctx_s,
+    st_object_t;
     st_window_t *window;
     EGLDisplay   display;
     EGLConfig    cfg;
@@ -63,6 +42,12 @@ ST_STRUCT_CLASS(st_gfxctx_s,
     bool         debug;
     st_dlist_t  *shared_data;
     st_htable_t *userdata;
-) st_gfxctx_t;
+} st_gfxctx_t;
 
+typedef struct {
+    st_gfxctx_t *ctx;
+    unsigned     index;
+} st_gfxctx_shared_data_t;
+
+#define ST_GFXCTXCTX_T_DEFINED
 #define ST_GFXCTX_T_DEFINED
