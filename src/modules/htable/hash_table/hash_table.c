e47b8d3931095072b0f85185c52c1aed5280a6da
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-12-25 13:38:25 +0600

    Fix htable module init func call

--- src/modules/htable/hash_table/hash_table.c
+++ src/modules/htable/hash_table/hash_table.c
@@ -66,9 +66,8 @@ st_moddata_t *st_module_htable_hash_table_init(st_modsmgr_t *modsmgr) {
 }
 
 #ifdef ST_MODULE_TYPE_shared
-st_moddata_t *st_module_init(st_modsmgr_t *modsmgr,
- st_modsmgr_funcs_t *modsmgr_funcs) {
-    return st_module_htable_hash_table_init(modsmgr, modsmgr_funcs);
+st_moddata_t *st_module_init(st_modsmgr_t *modsmgr) {
+    return st_module_htable_hash_table_init(modsmgr);
 }
 #endif
 
