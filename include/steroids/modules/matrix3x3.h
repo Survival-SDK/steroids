a7fca1020b30ac799cfe05710b8cd1ea836a23e3
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2025-12-25 15:50:00 +0600

    Update matrix3x3 module and remove old binding

--- include/steroids/modules/matrix3x3.h
+++ include/steroids/modules/matrix3x3.h
@@ -1,84 +1,89 @@
 #pragma once
 
-#include "steroids/module.h"
+#include "steroids/modctx.h"
 #include "steroids/object.h"
 
-#ifndef ST_MATRIX3X3_T_DEFINED
-    ST_CLASS (
-        float _private[6];
-    ) st_matrix3x3_t;
+#ifndef ST_MATRIX3X3CTX_T_DEFINED
+    typedef st_modctx_t st_matrix3x3ctx_t;
 #endif
 
-typedef st_modctx_t *(*st_matrix3x3_init_t)(st_modctx_t *logger_ctx,
- st_modctx_t *angle_ctx);
-typedef void (*st_matrix3x3_quit_t)(st_modctx_t *matrix3x3_ctx);
+typedef struct {
+    float r1c1, r1c2, r1c3;
+    float r2c1, r2c2, r2c3;
+    /*       0,    0,    1 */
+} st_matrix3x3_t;
 
-typedef void (*st_matrix3x3_clone_t)(const st_matrix3x3_t *matrix,
- st_matrix3x3_t *dst);
-typedef void (*st_matrix3x3_custom_t)(st_modctx_t *matrix3x3_ctx,
+typedef void (*st_matrix3x3_clone_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *dst, const st_matrix3x3_t *matrix);
+typedef void (*st_matrix3x3_custom_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
  st_matrix3x3_t *matrix, float r1c1, float r1c2, float r1c3, float r2c1,
  float r2c2, float r2c3);
-typedef void (*st_matrix3x3_identity_t)(st_modctx_t *matrix3x3_ctx,
+typedef void (*st_matrix3x3_identity_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
  st_matrix3x3_t *matrix);
-typedef void (*st_matrix3x3_translation_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float x, float y);
-typedef void (*st_matrix3x3_scaling_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float hscale, float vscale);
-typedef void (*st_matrix3x3_rrotation_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float radians);
-typedef void (*st_matrix3x3_drotation_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float degrees);
-typedef void (*st_matrix3x3_rhshearing_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float radians);
-typedef void (*st_matrix3x3_dhshearing_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float degrees);
-typedef void (*st_matrix3x3_rvshearing_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float radians);
-typedef void (*st_matrix3x3_dvshearing_t)(st_modctx_t *matrix3x3_ctx,
- st_matrix3x3_t *matrix, float degrees);
-typedef void (*st_matrix3x3_apply_t)(st_matrix3x3_t *matrix,
- const st_matrix3x3_t *other);
-typedef void (*st_matrix3x3_translate_t)(st_matrix3x3_t *matrix, float x,
+typedef void (*st_matrix3x3_translation_t)(
+ const st_matrix3x3ctx_t *matrix3x3_ctx, st_matrix3x3_t *matrix, float x,
  float y);
-typedef void (*st_matrix3x3_scale_t)(st_matrix3x3_t *matrix, float hscale,
- float vscale);
-typedef void (*st_matrix3x3_rrotate_t)(st_matrix3x3_t *matrix, float radians);
-typedef void (*st_matrix3x3_drotate_t)(st_matrix3x3_t *matrix, float degrees);
-typedef void (*st_matrix3x3_rhshear_t)(st_matrix3x3_t *matrix, float radians);
-typedef void (*st_matrix3x3_dhshear_t)(st_matrix3x3_t *matrix, float degrees);
-typedef void (*st_matrix3x3_rvshear_t)(st_matrix3x3_t *matrix, float radians);
-typedef void (*st_matrix3x3_dvshear_t)(st_matrix3x3_t *matrix, float degrees);
-typedef void (*st_matrix3x3_get_data_t)(const st_matrix3x3_t *matrix,
- float *r1c1, float *r1c2, float *r1c3, float *r2c1, float *r2c2, float *r2c3);
+typedef void (*st_matrix3x3_scaling_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float hscale, float vscale);
+typedef void (*st_matrix3x3_rrotation_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float radians);
+typedef void (*st_matrix3x3_drotation_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float degrees);
+typedef void (*st_matrix3x3_rhshearing_t)(
+ const st_matrix3x3ctx_t *matrix3x3_ctx, st_matrix3x3_t *matrix, float radians);
+typedef void (*st_matrix3x3_dhshearing_t)(
+ const st_matrix3x3ctx_t *matrix3x3_ctx, st_matrix3x3_t *matrix, float degrees);
+typedef void (*st_matrix3x3_rvshearing_t)(
+ const st_matrix3x3ctx_t *matrix3x3_ctx, st_matrix3x3_t *matrix, float radians);
+typedef void (*st_matrix3x3_dvshearing_t)(
+ const st_matrix3x3ctx_t *matrix3x3_ctx, st_matrix3x3_t *matrix, float degrees);
+typedef void (*st_matrix3x3_apply_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, const st_matrix3x3_t *other);
+typedef void (*st_matrix3x3_translate_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float x, float y);
+typedef void (*st_matrix3x3_scale_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float hscale, float vscale);
+typedef void (*st_matrix3x3_rrotate_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float radians);
+typedef void (*st_matrix3x3_drotate_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float degrees);
+typedef void (*st_matrix3x3_rhshear_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float radians);
+typedef void (*st_matrix3x3_dhshear_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float degrees);
+typedef void (*st_matrix3x3_rvshear_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float radians);
+typedef void (*st_matrix3x3_dvshear_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ st_matrix3x3_t *matrix, float degrees);
+typedef void (*st_matrix3x3_get_data_t)(const st_matrix3x3ctx_t *matrix3x3_ctx,
+ const st_matrix3x3_t *matrix, float *r1c1, float *r1c2, float *r1c3,
+ float *r2c1, float *r2c2, float *r2c3);
 
 typedef struct {
-    st_matrix3x3_init_t        matrix3x3_init;
-    st_matrix3x3_quit_t        matrix3x3_quit;
-    st_matrix3x3_custom_t      matrix3x3_custom;
-    st_matrix3x3_identity_t    matrix3x3_identity;
-    st_matrix3x3_translation_t matrix3x3_translation;
-    st_matrix3x3_scaling_t     matrix3x3_scaling;
-    st_matrix3x3_rrotation_t   matrix3x3_rrotation;
-    st_matrix3x3_drotation_t   matrix3x3_drotation;
-    st_matrix3x3_rhshearing_t  matrix3x3_rhshearing;
-    st_matrix3x3_dhshearing_t  matrix3x3_dhshearing;
-    st_matrix3x3_rvshearing_t  matrix3x3_rvshearing;
-    st_matrix3x3_dvshearing_t  matrix3x3_dvshearing;
+    st_modctx_funcs_t;
+    st_matrix3x3_clone_t       clone;
+    st_matrix3x3_custom_t      custom;
+    st_matrix3x3_identity_t    identity;
+    st_matrix3x3_translation_t translation;
+    st_matrix3x3_scaling_t     scaling;
+    st_matrix3x3_rrotation_t   rrotation;
+    st_matrix3x3_drotation_t   drotation;
+    st_matrix3x3_rhshearing_t  rhshearing;
+    st_matrix3x3_dhshearing_t  dhshearing;
+    st_matrix3x3_rvshearing_t  rvshearing;
+    st_matrix3x3_dvshearing_t  dvshearing;
+    st_matrix3x3_apply_t       apply;
+    st_matrix3x3_translate_t   translate;
+    st_matrix3x3_scale_t       scale;
+    st_matrix3x3_rrotate_t     rrotate;
+    st_matrix3x3_drotate_t     drotate;
+    st_matrix3x3_rhshear_t     rhshear;
+    st_matrix3x3_dhshear_t     dhshear;
+    st_matrix3x3_rvshear_t     rvshear;
+    st_matrix3x3_dvshear_t     dvshear;
+    st_matrix3x3_get_data_t    get_data;
 } st_matrix3x3ctx_funcs_t;
 
-typedef struct {
-    st_matrix3x3_clone_t     clone;
-    st_matrix3x3_apply_t     apply;
-    st_matrix3x3_translate_t translate;
-    st_matrix3x3_scale_t     scale;
-    st_matrix3x3_rrotate_t   rrotate;
-    st_matrix3x3_drotate_t   drotate;
-    st_matrix3x3_rhshear_t   rhshear;
-    st_matrix3x3_dhshear_t   dhshear;
-    st_matrix3x3_rvshear_t   rvshear;
-    st_matrix3x3_dvshear_t   dvshear;
-    st_matrix3x3_get_data_t  get_data;
-} st_matrix3x3_funcs_t;
-
-#define ST_MATRIX3X3_CALL(object, func, ...) \
-    ((st_matrix3x3_funcs_t *)((const st_object_t *)object)->funcs)->func(object, ## __VA_ARGS__)
+#define ST_MATRIX3X3CTX_CALL(ctx, func, ...) \
+    ((st_matrix3x3ctx_funcs_t *)((const st_object_t *)ctx)->funcs)->func(ctx, \
+     ## __VA_ARGS__)
--- src/modules/matrix3x3/simple/simple.h
+++ src/modules/matrix3x3/simple/simple.h
@@ -2,46 +2,4 @@
 
 #include "config.h" // IWYU pragma: keep
 #include "types.h" // IWYU pragma: keep
-#include "steroids/matrix3x3.h"
-
-static st_matrix3x3ctx_funcs_t st_matrix3x3_simple_funcs = {
-    .matrix3x3_init        = st_matrix3x3_init,
-    .matrix3x3_quit        = st_matrix3x3_quit,
-    .matrix3x3_custom      = st_matrix3x3_custom,
-    .matrix3x3_identity    = st_matrix3x3_identity,
-    .matrix3x3_translation = st_matrix3x3_translation,
-    .matrix3x3_scaling     = st_matrix3x3_scaling,
-    .matrix3x3_rrotation   = st_matrix3x3_rrotation,
-    .matrix3x3_drotation   = st_matrix3x3_drotation,
-    .matrix3x3_rhshearing  = st_matrix3x3_rhshearing,
-    .matrix3x3_dhshearing  = st_matrix3x3_dhshearing,
-    .matrix3x3_rvshearing  = st_matrix3x3_rvshearing,
-    .matrix3x3_dvshearing  = st_matrix3x3_dvshearing,
-};
-
-static st_modfuncentry_t st_module_matrix3x3_simple_funcs[] = {
-    {"init",        st_matrix3x3_init},
-    {"quit",        st_matrix3x3_quit},
-    {"clone",       st_matrix3x3_clone},
-    {"custom",      st_matrix3x3_custom},
-    {"identity",    st_matrix3x3_identity},
-    {"translation", st_matrix3x3_translation},
-    {"scaling",     st_matrix3x3_scaling},
-    {"rrotation",   st_matrix3x3_rrotation},
-    {"drotation",   st_matrix3x3_drotation},
-    {"rhshearing",  st_matrix3x3_rhshearing},
-    {"dhshearing",  st_matrix3x3_dhshearing},
-    {"rvshearing",  st_matrix3x3_rvshearing},
-    {"dvshearing",  st_matrix3x3_dvshearing},
-    {"apply",       st_matrix3x3_apply},
-    {"translate",   st_matrix3x3_translate},
-    {"scale",       st_matrix3x3_scale},
-    {"rrotate",     st_matrix3x3_rrotate},
-    {"drotate",     st_matrix3x3_drotate},
-    {"rhshear",     st_matrix3x3_rhshear},
-    {"dhshear",     st_matrix3x3_dhshear},
-    {"rvshear",     st_matrix3x3_rvshear},
-    {"dvshear",     st_matrix3x3_dvshear},
-    {"get_data",    st_matrix3x3_get_data},
-    {NULL, NULL},
-};
+#include "steroids/modules/matrix3x3.h"
